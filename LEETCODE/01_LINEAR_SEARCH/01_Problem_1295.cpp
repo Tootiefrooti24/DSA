ca3617d319e4139be6eda02fdeed1a3e561e36c2
Tootiefrooti24 <pshrutz24@gmail.com>
2026-03-26 16:59:42 +0000

    Find Number with even number of digits

---
+++ LEETCODE/01_LINEAR_SEARCH/01_Problem_1295.cpp
@@ -0,0 +1,21 @@
+//Given an array nums of integers, return how many of them contain an even number of digits.
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int n=nums.size();
+        int even=0;
+        for(int i=0;i<n;i++){
+            int num=nums[i];
+            int count=0;
+            while(num>0){
+                num=num/10;
+                count++;
+
+            }
+            if(count%2==0){
+                even++;
+            }
+        }
+        return even;
+    }
+};
