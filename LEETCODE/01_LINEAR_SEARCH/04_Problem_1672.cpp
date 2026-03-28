d3f724501b3f715566b01be8c284d5cfd57c6655
Tootiefrooti24 <pshrutz24@gmail.com>
2026-03-28 12:14:58 +0000

    Richest Customer wealth-1672

---
+++ LEETCODE/01_LINEAR_SEARCH/04_Problem_1672.cpp
@@ -0,0 +1,20 @@
+//Richest Customer wealth
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int m=accounts.size();
+        int n=accounts[0].size();
+        int max=INT_MIN;
+        for(int i=0;i<m;i++){
+        int sum=0;
+            for(int j=0;j<n;j++){
+                sum=sum+accounts[i][j];
+            }
+            if(max<sum){
+                max=sum;
+            }
+        }
+        return max;
+    }
+};
