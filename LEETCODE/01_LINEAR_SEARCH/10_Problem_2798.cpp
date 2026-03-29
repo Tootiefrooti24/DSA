5d6406643f5860c061e2803fccc0b2bfdc307e40
Tootiefrooti24 <pshrutz24@gmail.com>
2026-03-29 07:45:47 +0000

    Number of employees who met the target - 2798

---
+++ LEETCODE/01_LINEAR_SEARCH/10_Problem_2798.cpp
@@ -0,0 +1,14 @@
+//Number of employees who met the target
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int n=hours.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+          if(hours[i]>=target){
+            count++;
+          }
+        }
+        return count;
+    }
+};
