b826305ec8a5629ac54f49583f542c19e6a4f4b4
Tootiefrooti24 <pshrutz24@gmail.com>
2026-04-03 18:07:39 +0000

    Added info about my brute force approach

--- LEETCODE/01_LINEAR_SEARCH/16_Problem_1732.cpp
+++ LEETCODE/01_LINEAR_SEARCH/16_Problem_1732.cpp
@@ -1,5 +1,6 @@
 //Find the Highest Altitude
-//More than linear search it is about running sum and tracking maximum
+
+//More than linear search it is about running sum and tracking maximum.The alternate way using which i solved it was using loops and traversing through the loop and updating gain[i] =gain[i]+ gain[i+1] and altitude[i+1]=gain[i] and then checking the max value among them but as it was running sum and checking maximum value ,the optimal solution is below one
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
